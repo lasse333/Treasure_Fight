85bc04444ea5fc8bb8fb0e4992759e3a30f29fcb
lasse333 <reneskjodt333@gmail.com>
2023-08-06 10:55:59 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,7 +3,15 @@
 
 // Var and events
 
-
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
 
 var canvas = document.getElementById("myCanvas")
 var ctx = canvas.getContext("2d")
